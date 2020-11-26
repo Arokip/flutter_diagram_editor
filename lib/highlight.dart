8a4519c71643f53344ce9441f8886a34a4eff6a6
unknown <arokip.quique@gmail.com>
2020-11-26 23:01:38 +0100

    improved dashed highlight again

--- lib/highlight.dart
+++ lib/highlight.dart
@@ -62,6 +62,18 @@ class HighlightPainter extends CustomPainter {
       ..strokeWidth = strokeWidth
       ..style = PaintingStyle.stroke;
 
+    if (dashWidth <= 0 || dashSpace <= 0) {
+      canvas.drawRect(
+          Rect.fromLTWH(
+            this.position.dx,
+            this.position.dy,
+            this.width,
+            this.height,
+          ),
+          paint);
+      return;
+    }
+
     Path dashedPath = Path();
 
     var width = this.width + strokeWidth;
@@ -70,50 +82,50 @@ class HighlightPainter extends CustomPainter {
     var position = this.position - Offset(strokeWidth / 2, 0);
     double pathLength = 0;
 
-    if (dashWidth >= width) {
+    if (dashWidth + 2 * dashSpace >= width) {
       dashedPath.moveTo(position.dx, position.dy);
       dashedPath.lineTo(position.dx + width, position.dy);
 
       dashedPath.moveTo(position.dx, this.height + position.dy);
       dashedPath.lineTo(position.dx + width, this.height + position.dy);
-    }
+    } else {
+      while (pathLength < width) {
+        double nextX = (pathLength + dashWidth < width)
+            ? position.dx + pathLength + dashWidth
+            : position.dx + width;
+        dashedPath.moveTo(position.dx + pathLength, position.dy);
+        dashedPath.lineTo(nextX, position.dy);
 
-    while (pathLength < width) {
-      double nextX = (pathLength + dashWidth < width)
-          ? position.dx + pathLength + dashWidth
-          : position.dx + width;
-      dashedPath.moveTo(position.dx + pathLength, position.dy);
-      dashedPath.lineTo(nextX, position.dy);
+        dashedPath.moveTo(position.dx + pathLength, this.height + position.dy);
+        dashedPath.lineTo(nextX, this.height + position.dy);
 
-      dashedPath.moveTo(position.dx + pathLength, this.height + position.dy);
-      dashedPath.lineTo(nextX, this.height + position.dy);
-
-      pathLength = pathLength + dashWidth + dashSpace;
+        pathLength = pathLength + dashWidth + dashSpace;
+      }
     }
 
     position = this.position - Offset(0, strokeWidth / 2);
     pathLength = 0;
 
-    if (dashWidth >= height) {
+    if (dashWidth + 2 * dashSpace >= height) {
       dashedPath.moveTo(position.dx, position.dy);
       dashedPath.lineTo(position.dx, height + position.dy);
 
       dashedPath.moveTo(this.width + position.dx, position.dy);
       dashedPath.lineTo(this.width + position.dx, height + position.dy);
-    }
+    } else {
+      while (pathLength < height) {
+        double nextY = (pathLength + dashWidth < height)
+            ? position.dy + pathLength + dashWidth
+            : position.dy + height;
 
-    while (pathLength < height) {
-      double nextY = (pathLength + dashWidth < height)
-          ? position.dy + pathLength + dashWidth
-          : position.dy + height;
+        dashedPath.moveTo(position.dx, position.dy + pathLength);
+        dashedPath.lineTo(position.dx, nextY);
 
-      dashedPath.moveTo(position.dx, position.dy + pathLength);
-      dashedPath.lineTo(position.dx, nextY);
+        dashedPath.moveTo(this.width + position.dx, position.dy + pathLength);
+        dashedPath.lineTo(this.width + position.dx, nextY);
 
-      dashedPath.moveTo(this.width + position.dx, position.dy + pathLength);
-      dashedPath.lineTo(this.width + position.dx, nextY);
-
-      pathLength = pathLength + dashWidth + dashSpace;
+        pathLength = pathLength + dashWidth + dashSpace;
+      }
     }
 
     canvas.drawPath(dashedPath, paint);
